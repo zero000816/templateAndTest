8eb0d8fd72adece2ccad1a89034e7dbee6bdf0dd
zero <1055659897@qq.com>
2022-01-03 22:05:57 +0800

    first commit

---
+++ src/main/java/com/cy/bigplan/BigplanApplication.java
@@ -0,0 +1,13 @@
+package com.cy.bigplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cy/bigplan/BigplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.bigplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
